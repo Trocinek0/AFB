76091c8972b2ff8588c2898e2cca3a8dbbf1eea6
tr0 <spoko00100@gmail.com>
2022-07-09 12:58:43 +0200

    fix after fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,12 @@
 import react from 'react'
-import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav className=" px-2 py-2.5 bg-emerald-500 sm:px-4">
+    <nav className=" px-2 py-2.5 bg-teal-700 sm:px-4">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
-        <Link to="/" className="flex items-center">
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
             Among Us Fanbase
           </span>
-        </Link>
       </div>
     </nav>
   )
